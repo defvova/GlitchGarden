80312d289c26e5b0852ce3825db2a8cc53f81638
Vova <volodimir.partytskyi@gmail.com>
2020-07-06 17:51:01 +0300

    Add Splash Screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float loadTime = 3f;
+
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadNextScene());
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(loadTime);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
---
+++ Assets/Scripts/LoadSound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSound : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int loadSoundLength = FindObjectsOfType<LoadSound>().Length;
+        if (loadSoundLength > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
