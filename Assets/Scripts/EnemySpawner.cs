bd55802788a45bfce9c99a348130dced1871d5d2
Vova <volodimir.partytskyi@gmail.com>
2020-07-08 15:26:33 +0300

    Add enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private float minTimeSpawner = 1f;
+    [SerializeField] private float maxTimeSpawner = 5f;
+    [SerializeField] private bool isSpawner = true;
+    [SerializeField] private GameObject enemy;
+
+    private IEnumerator Start()
+    {
+        while (isSpawner)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minTimeSpawner, maxTimeSpawner));
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
+}
