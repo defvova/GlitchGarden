e2bde711124e2da79afeffce38f250c3bcccb752
Vova <volodimir.partytskyi@gmail.com>
2020-07-08 14:20:19 +0300

    Add lizard movement

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 2f)] [SerializeField] private float walkSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
