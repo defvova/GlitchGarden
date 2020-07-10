bdc0a4e788b89e174f0ce14ecc0dc21541b60079
Vova <volodimir.partytskyi@gmail.com>
2020-07-10 20:02:41 +0300

    Add resources as anim event

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int starCost = 100;
+
+    private StarDisplay starDisplay;
+
+    private void Start()
+    {
+        starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    public void AddingStars(int amount)
+    {
+        starDisplay.AddingStars(amount);
+    }
 }
