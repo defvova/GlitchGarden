ac4cccd6d2058bdf1132491e9a1294ff88c7b1c1
Vova <volodimir.partytskyi@gmail.com>
2020-07-10 17:15:17 +0300

    Display stars count

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] private int currentStars = 100;
+    private Text starDisplay;
+
+    private void Start()
+    {
+        starDisplay = GetComponent<Text>();
+        UpdateStarsText();
+    }
+
+    private void UpdateStarsText()
+    {
+        starDisplay.text = currentStars.ToString();
+    }
+
+    public void AddingStars(int amount)
+    {
+        currentStars += amount;
+        UpdateStarsText();
+    }
+
+    public void SpendingStars(int amount)
+    {
+        if (currentStars >= amount)
+        {
+            currentStars -= amount;
+            UpdateStarsText();
+        }
+    }
+}
