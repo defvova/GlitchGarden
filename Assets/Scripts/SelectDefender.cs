8f68b2d6bf6bf59d341bb2b93ebf72630fe11fe1
Vova <volodimir.partytskyi@gmail.com>
2020-07-10 15:14:01 +0300

    Add defender selection UI

---
+++ Assets/Scripts/SelectDefender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDefender : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var defenders = FindObjectsOfType<SelectDefender>();
+        foreach (SelectDefender defender in defenders)
+        {
+            defender.GetComponent<SpriteRenderer>().color = new Color32(46, 46, 46, 255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
