656391b67d23a9525f56350e644dd2c300be233f
Vova <volodimir.partytskyi@gmail.com>
2020-07-12 14:28:58 +0300

    Attacker in my lane & Change animation state state

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,9 +6,43 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
     [SerializeField] private GameObject gun;
+    private Animator animator;
+
+    private EnemySpawner myLaneSpawner;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        animator.SetBool("IsAttacking", IsAttackingInLane());
+    }
 
     public void Fire()
     {
         Instantiate(bullet, gun.transform.position, gun.transform.rotation);
+    }
+
+    private void SetLaneSpawner()
+    {
+        EnemySpawner[] spawners = FindObjectsOfType<EnemySpawner>();
+
+        foreach (EnemySpawner spawner in spawners)
+        {
+            if (GetIsCloseEnough(spawner)) myLaneSpawner = spawner;
+        }
+    }
+
+    private bool GetIsCloseEnough(EnemySpawner spawner)
+    {
+        return Mathf.Abs(Mathf.RoundToInt(spawner.transform.position.y) - Mathf.RoundToInt(transform.position.y)) <= Mathf.Epsilon;
+    }
+
+    private bool IsAttackingInLane()
+    {
+        return myLaneSpawner && myLaneSpawner.transform.childCount > 0;
     }
 }
